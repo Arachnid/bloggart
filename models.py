79878dc87fac43d45a614966f35552b3f09d7ef7
Nick Johnson <arachnid@notdot.net>
2009-10-20 22:09:27 +0100

    Fix tag escaping. Fixes #15

--- models.py
+++ models.py
@@ -26,6 +26,10 @@ class BlogPost(db.Model):
     return list(set(utils.slugify(x.lower()) for x in tags))
 
   @property
+  def tag_pairs(self):
+    return zip(self.tags, self.normalized_tags)
+
+  @property
   def summary(self):
     """Returns a summary of the blog post."""
     match = re.search("<!--.*cut.*-->", self.body)
