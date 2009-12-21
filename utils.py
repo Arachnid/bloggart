13645c4903bed2a696bd7e45ef97acfb04eaf963
Anh Hai Trinh <anh.hai.trinh@gmail.com>
2009-12-21 19:13:04 +0800

    Fix utils.render_template to work when imported from a non-root path, e.g. /script

--- utils.py
+++ utils.py
@@ -11,14 +11,15 @@ from django.template import loader
 
 import config
 
+BASE_DIR = os.path.dirname(__file__)
 
 if isinstance(config.theme, (list, tuple)):
   TEMPLATE_DIRS = config.theme
 else:
-  TEMPLATE_DIRS = [os.path.abspath('themes/default')]
+  TEMPLATE_DIRS = [os.path.abspath(os.path.join(BASE_DIR, 'themes/default'))]
   if config.theme and config.theme != 'default':
     TEMPLATE_DIRS.insert(0,
-                         os.path.abspath(os.path.join('themes', config.theme)))
+                         os.path.abspath(os.path.join(BASE_DIR, 'themes', config.theme)))
 
 
 def slugify(s):
