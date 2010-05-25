24cb6aa2d7c084836758e094abcb054a4af16996
Tay Ray Chuan <rctay89@gmail.com>
2010-05-25 13:17:15 +0800

    post_deply: don't compare Model with tuple
    Create a tuple from the Model first before comparing with
    BLOGGART_VERSION.

--- post_deploy.py
+++ post_deploy.py
@@ -51,7 +51,13 @@ post_deploy_tasks.append(generate_static_pages([
 
 
 def regenerate_all(previous_version):
-  if previous_version < BLOGGART_VERSION:
+  if previous_version:
+    ver_tuple = (
+      previous_version.bloggart_major,
+      previous_version.bloggart_minor,
+      previous_version.bloggart_rev,
+    )
+  if ver_tuple < BLOGGART_VERSION:
     regen = PostRegenerator()
     deferred.defer(regen.regenerate)
 
