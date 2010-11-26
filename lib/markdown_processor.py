2595ac9a66a4c1d1a458fc8a6dcfd35349682fb1
Ivan De Marino <ivan.de.marino@gmail.com>
2010-11-26 14:18:48 +0000

    Added 'line numbers' to 'sourcecode' areas! Finally :)

--- lib/markdown_processor.py
+++ lib/markdown_processor.py
@@ -53,7 +53,7 @@ class CodeBlockPreprocessor(TextPreprocessor):
     pattern = re.compile(
         r'\s*\[sourcecode:(.+?)\](.+?)\[/sourcecode\]\s*', re.S)
 
-    formatter = HtmlFormatter(noclasses=INLINESTYLES, lineseparator=LINEENDING)
+    formatter = HtmlFormatter(noclasses=INLINESTYLES, lineseparator=LINEENDING, linenos='inline')
 
     def run(self, lines):
         def repl(m):
